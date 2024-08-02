aba2755caef1d849df5274f3b81090ea3afa70bc
Aneesfathima-786 <20eucs009@skcet.ac.in>
2024-08-02 19:06:12 +0530

    3D Visual Learn

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="flex bg-gray-900 text-white justify-center text-xl py-3 items-center w-full mx-auto z-50">
+            Copyright &copy; Visual3D - Innovation Lab {new Date().getFullYear()}
+        </div>
+    )
+}
